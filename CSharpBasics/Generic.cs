44ebec64342c5e669e76a7afa0aee6ec51009cf3
Susan Shrestha <susan.shrestha2056@gmail.com>
2020-10-12 22:37:40 +0545

    Learned ablut generic methods

---
+++ CSharpBasics/Generic.cs
@@ -0,0 +1,8 @@
+// Generics -> It is used so that every data type can be used in a method/function
+public class Calculator
+{
+    public static T First<T>(T firstNum, T secondNum)
+    {
+        return firstNum;
+    }
+}
